4a95a965ce7622741202f6f09162b092172e2ad7
craftwar <craftwar@users.noreply.github.com>
2018-01-23 15:34:47 +0800

    Improve geting exeName
    use GetProcessImageFileName only

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ add features that aren't in pssuspend.exe: unicode support, mutiple target proce
 Suspend processes which are not needed to save CPU usage.  
 ex: suspend a game to play another game smoothly or use as fast mute. (suspend the other game when lol starts)  
 
-[lastest build from appveyor](https://ci.appveyor.com/api/projects/craftwar_appveyor/suspend/artifacts/suspend.7z) (when I am lazy to make realease on github) 
+[lastest build from appveyor](https://ci.appveyor.com/api/projects/craftwar_appveyor/suspend/artifacts/suspend.7z?branch=master) (when I am lazy to make realease on github) 
 
 If you get missing dll error, try to install [Visual C++ Redistributable for Visual Studio 2017](https://go.microsoft.com/fwlink/?LinkId=746571)  
--- suspend/main.cpp
+++ suspend/main.cpp
@@ -1,22 +1,30 @@
 #include <QCoreApplication>
 #include <QStringList>
 #include <QCommandLineParser>
-#include <QDebug>
+#ifdef QT_DEBUG
+    #include <QDebug>
+#endif
 #include <QTextStream>
 
 #include <locale>
 #include <iostream>
 
 #ifdef WIN32
-#define _UNICODE
+
+#define WINVER 0x0601
+#define _WIN32_WINNT 0x0601
+#ifndef _UNICODE
+	#define _UNICODE
+#endif
+
 #include <qt_windows.h>
 #include <WinNT.h>
 #include <Psapi.h>
 #include <tchar.h>
 #include <fcntl.h>
 #include <io.h>
-#endif
 
+#endif
 
 typedef LONG (NTAPI *NtSuspendProcess)(IN HANDLE ProcessHandle);
 typedef LONG (NTAPI *NtResumeProcess)(IN HANDLE ProcessHandle);
@@ -34,7 +42,6 @@ typedef LONG (NTAPI *NtResumeProcess)(IN HANDLE ProcessHandle);
 using namespace std;
 
 
-
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -45,8 +52,12 @@ int main(int argc, char *argv[])
     Qt::CaseSensitivity bCaseSensitivity;
     TCHAR szProcessName[MAX_PATH];
     _TCHAR* exeName;
-    DWORD aProcesses[1024], cbNeeded, cProcesses, result;
+    DWORD aProcesses[1024], cbNeeded, cProcesses;
+//    DWORD result;
+    DWORD *Process_cur = aProcesses + 1; // skip System Idle Process
+    DWORD *Process_end; //not included
     HANDLE hProcess;
+
 // 1) set locale to fix wcout but not working
 //    std::locale::global(std::locale(""));
 //    std::locale::global(std::locale("Japan"));
@@ -55,6 +66,7 @@ int main(int argc, char *argv[])
 //	SetConsoleOutputCP(65001);
 // 2) _setmode range wtext > u16text?
 //    _setmode(_fileno(stdout), _O_U16TEXT);
+
     _setmode(_fileno(stdout), _O_WTEXT);
 
     //TODO
@@ -84,6 +96,9 @@ int main(int argc, char *argv[])
 //    bool bPID = parser.isSet(showProgressOption);
 
     QStringList nameList = parser.positionalArguments();
+#ifdef QT_DEBUG
+	qDebug() << nameList << endl;
+#endif
 
 
     // Get the list of process identifiers.
@@ -93,70 +108,16 @@ int main(int argc, char *argv[])
     }
 
     // Calculate how many process identifiers were returned.
-
     cProcesses = cbNeeded / sizeof(DWORD);
     if (cbNeeded > sizeof(aProcesses))
     {
         std::wcout <<" need more size to save process identifiers\n";
         return 1;
     }
-
-    // Print the name and process identifier for each process.
-
-
-////****
-
-//hProcess = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION |PROCESS_SUSPEND_RESUME, false, 4792);
-////hProcess = OpenProcess(PROCESS_QUERY_INFORMATION |PROCESS_SUSPEND_RESUME |PROCESS_VM_READ, false, 4792);
+    Process_end = aProcesses+cProcesses;
 
 
 
-
-//result = GetProcessImageFileName(hProcess, szProcessName, MAX_PATH);
-//std::wcout << "result: " << result << endl;
-//std::wcout << szProcessName << endl;
-
-
-//if (hProcess &&
-//        QueryFullProcessImageName(hProcess, 0, szProcessName, &result)
-//         )
-//{
-
-//    std::wcout << "result: " << result << endl;
-//    std::wcout << szProcessName << endl;
-//    exeName = _tcsrchr(szProcessName, '\\');
-//    //int index = strProcessName.lastIndexOf('\\');
-//    std::wcout << "exeName: " << ++exeName << endl;
-//    if ( nameList.contains( QString::fromWCharArray(exeName) ))
-//    {
-//        std::wcout <<"find" << endl;
-//    }
-//    else
-//        std::wcout <<"not find" << endl;
-//}
-//else
-//{
-//    std::wcout << "result: " <<result <<endl;
-//    std::wcout << "lasterror: " <<GetLastError() <<endl;
-//}
-////qDebug() << "debug test: "<< szProcessName <<endl;
-
-//if (NULL != hProcess )
-//{
-//    HMODULE hMod;
-//    DWORD cbNeeded;
-
-//    if ( EnumProcessModules( hProcess, &hMod, sizeof(hMod),
-//         &cbNeeded) )
-//    {
-//        GetModuleBaseName( hProcess, hMod, szProcessName,
-//                           sizeof(szProcessName)/sizeof(TCHAR) );
-//    }
-//    std::wcout <<"szProcessName from GetModuleBaseName: " <<szProcessName << endl;
-//}
-//CloseHandle(hProcess);
-
-//****
 
     if (bNoOp)
         std::wcout <<"no operation mode"<< endl;
@@ -168,45 +129,40 @@ int main(int argc, char *argv[])
     {
         std::wcout <<"suspended process(es):"<< endl;
     }
-    for (unsigned int i = 0; i < cProcesses; i++ )
+    for ( ; Process_cur< Process_end; ++Process_cur)
     {
-        if( aProcesses[i] != 0 )
+        hProcess = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION | PROCESS_SUSPEND_RESUME, false, *Process_cur);
+        if (hProcess)
         {
-            hProcess = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION | PROCESS_SUSPEND_RESUME, false, aProcesses[i]);
-            if (hProcess)
+            //BUG:  call GetProcessImageFileName() before QueryFullProcessImageName() or QueryFullProcessImageName() doesn't work properly
+            //      That's why MSDN use strange exampe to get process name.
+//            result = GetProcessImageFileName(hProcess, szProcessName, MAX_PATH);
+//            if ( result && QueryFullProcessImageName(hProcess, 0, szProcessName, &result) )
+
+            // use GetProcessImageFileName only (don't get full file path, use ImageFileName only)
+            if ( GetProcessImageFileName(hProcess, szProcessName, MAX_PATH) )
             {
-                //BUG:  call GetProcessImageFileName() before QueryFullProcessImageName() or QueryFullProcessImageName() doesn't work properly
-                //      That's why MSDN use strange exampe to get process name.
-                result = GetProcessImageFileName(hProcess, szProcessName, MAX_PATH);
-//                result =1;
-                if ( result && QueryFullProcessImageName(hProcess, 0, szProcessName, &result) )
+                exeName = _tcsrchr(szProcessName, '\\') + 1;
+                if ( exeName && nameList.contains( QString::fromWCharArray(exeName), bCaseSensitivity ))
                 {
-                    exeName = _tcsrchr(szProcessName, '\\');
-//                    std::wcout << "szProcessName: "<< szProcessName <<endl;
-//                        std::wcout << "exeName: " << ++exeName << endl;
-                    if ( exeName++ && nameList.contains( QString::fromWCharArray(exeName), bCaseSensitivity ))
+                    if (!bNoOp)
                     {
-//                        qDebug() << nameList << endl;
-//                        wcout <<"find" << endl;
-                        if (!bNoOp)
+                        if (bResume)
                         {
-                            if (bResume)
-                            {
-                                if (!pfnNtResumeProcess(hProcess))
-                                    std::wcout << exeName << endl;
-                            }
-                            else
-                            {
-                                if(!pfnNtSuspendProcess(hProcess))
-                                    std::wcout << exeName << endl;
-                            }
+                            if (!pfnNtResumeProcess(hProcess))
+                                std::wcout << exeName << endl;
                         }
                         else
-                            std::wcout << exeName << endl;
+                        {
+                            if(!pfnNtSuspendProcess(hProcess))
+                                std::wcout << exeName << endl;
+                        }
                     }
-				}
-            }
-            CloseHandle(hProcess);
+                    else
+                        std::wcout << exeName << endl;
+                }
+			}
+		CloseHandle(hProcess);
         }
     }
 
